59b1b8ad0e541d6d766eb62bde6e7290c797f7c0
mingkun <mingkun502@gmail.com>
2024-12-06 18:51:18 +0800

    feat: file operation

--- utils/file_operation.py
+++ utils/file_operation.py
@@ -12,3 +12,12 @@ def _create_folder_if_not_exists(folder_path: str):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         print(f"created folder: {folder_path}")
+
+
+def _list_all_files(dir_path: str):
+    file_list = []
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_list.append(file_path)
+    return file_list
