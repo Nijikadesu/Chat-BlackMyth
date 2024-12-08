b3d38fdc430eef8de25452b65343172aa91196b1
mingkun <mingkun502@gmail.com>
2024-12-08 14:02:28 +0800

    modify .gitignore

--- download_data/get_data_from_url.py
+++ download_data/get_data_from_url.py
@@ -46,37 +46,33 @@ def get_description(url):
 
 def write2md(role, level, description):
     # 定义变量，创建目录与文件
-    file_name = f"{role}.md"
-    dir_name = f"../data/portraits/{level}"
-    markdown_file = os.path.join(dir_name, file_name)
-    _create_folder_if_not_exists(dir_name)
+    markdown_file = "../data/portraits.md"
     _create_file_if_not_exists(markdown_file)
 
     # 生成 Markdown 内容（Markdown模板内容需要置前排列,否则写入文件会以代码块展示）
     markdown_content = f"""
-## 类型
+## 影神图角色
+
+{role}
+
+### 类型
 
 {level}
 
-## 描述
+### 描述
 
 {description}
     """
 
     # 将 Markdown 内容写入文件
-    with open(markdown_file, "w", encoding="utf-8") as file:
+    with open(markdown_file, "a", encoding="utf-8") as file:
         file.write(markdown_content)
-    print(f"Markdown 文件已生成: {markdown_file}")
 
 
 if __name__ == '__main__':
-    # description = get_description('https://www.gamersky.com/handbook/202408/1803371_24.shtml')
-    # role = '白衣秀士'
-    # level = '人物'
-    # write2md(role, level, description)
 
-    _create_folder_if_not_exists('../data/portraits')
-    roleinfo_config = r"../workspace/roleinfo.json"
+    # _create_folder_if_not_exists('../data/portraits')
+    roleinfo_config = r"../load_data/roleinfo.json"
     with open (roleinfo_config, 'r', encoding='utf-8') as file:
         roleinfo = file.read()
     roleinfo_dict = json.loads(roleinfo)
@@ -85,6 +81,6 @@ if __name__ == '__main__':
         print(f"类型: {level}")
         for item in items:
             role = item['role']
-            url= item['url']
+            url = item['url']
             description = get_description(url)
             write2md(role, level, description)
